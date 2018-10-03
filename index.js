b1c61ab265fb0bdd04de41cbafa78dc48d794f6d
Tiffany Carter <tafby88@gmail.com>
2018-10-03 00:02:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ function sayHiToGrandma(string) {
   return "I love you, too."
 }
   
-  function whisper(string) {
+function whisper(string) {
     return string.toLowerCase();
   }
-  function logShout(string) {
+function logShout(string) {
     console.log(string.toUpperCase());
   }
   
-  function logWhisper(string) {
+function logWhisper(string) {
     console.log(string.toLowerCase());
   }
-  
-}
