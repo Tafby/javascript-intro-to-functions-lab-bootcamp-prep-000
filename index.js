b39d0134b273e9aa8f6352be0d3e4012885166ea
Tiffany Carter <tafby88@gmail.com>
2018-10-03 00:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ function sayHiToGrandma(string) {
   }
   whisper(hello)
   function logShout(string) {
-    return console.log(string.toUpperCase());
-
+    console.log(string.toUpperCase());
   }
   
   function logWhisper(string) {
     console.log(string.toLowerCase());
-    string
   }
   
 }
